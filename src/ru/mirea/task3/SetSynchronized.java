b73a57b1b519e252cc81994cbcfffd9fff76dfd8
mclyashko <kenuay007@gmail.com>
2022-02-24 18:19:21 +0300

    synchronized set hotfix

--- src/ru/mirea/task3/SetSynchronized.java
+++ src/ru/mirea/task3/SetSynchronized.java
@@ -10,68 +10,68 @@ public class SetSynchronized<K> implements Set<K> {
     private final Set<K> mySet = new HashSet<>();
 
     @Override
-    public int size() {
+    synchronized public int size() {
         return mySet.size();
     }
 
     @Override
-    public boolean isEmpty() {
+    synchronized public boolean isEmpty() {
         return mySet.isEmpty();
     }
 
     @Override
-    public boolean contains(Object o) {
+    synchronized public boolean contains(Object o) {
         return mySet.contains(o);
     }
 
     @Override
-    public Iterator<K> iterator() {
+    synchronized public Iterator<K> iterator() {
         return mySet.iterator();
     }
 
     @Override
-    public Object[] toArray() {
+    synchronized public Object[] toArray() {
         return mySet.toArray();
     }
 
     @Override
     @SuppressWarnings("all")
-    public <T> T[] toArray(T[] a) {
+    synchronized public <T> T[] toArray(T[] a) {
         return mySet.toArray(a);
     }
 
     @Override
-    public boolean add(K k) {
+    synchronized public boolean add(K k) {
         return mySet.add(k);
     }
 
     @Override
-    public boolean remove(Object o) {
+    synchronized public boolean remove(Object o) {
         return mySet.remove(o);
     }
 
     @Override
-    public boolean containsAll(Collection<?> c) {
+    synchronized public boolean containsAll(Collection<?> c) {
         return mySet.containsAll(c);
     }
 
     @Override
-    public boolean addAll(Collection<? extends K> c) {
+    synchronized public boolean addAll(Collection<? extends K> c) {
         return mySet.addAll(c);
     }
 
     @Override
-    public boolean retainAll(Collection<?> c) {
+    synchronized public boolean retainAll(Collection<?> c) {
         return mySet.retainAll(c);
     }
 
     @Override
-    public boolean removeAll(Collection<?> c) {
+    synchronized public boolean removeAll(Collection<?> c) {
         return mySet.removeAll(c);
     }
 
     @Override
-    public void clear() {
+    synchronized public void clear() {
         mySet.clear();
     }
 }
