8285ff41b29768128cbd3c512f8e53a4dabd8730
Victor Lyashenko <kenuay007@gmail.com>
2022-05-06 22:27:11 +0300

    task 18 done

---
+++ nextTasks/task18/src/test/java/ru/mirea/task18/Task18ApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.mirea.task18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SuppressWarnings("EmptyMethod")
+@SpringBootTest
+class Task18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
